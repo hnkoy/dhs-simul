98b605320418f01a801747052fe7435cf734023e
hnkoy <henocknkoy9@gmail.com>
2026-02-10 08:58:49 +0100

    committ debug openfn

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AgregateDhs2Controller;
 
 Route::post('/agregate-dhs2', [AgregateDhs2Controller::class, 'store']);
+Route::post('/debug-body', function (\Illuminate\Http\Request $request) {
+    return response()->json([
+        'headers' => $request->headers->all(),
+        'raw' => $request->getContent(),
+        'parsed' => $request->all(),
+    ]);
+});
